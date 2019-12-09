9b57dfeb8e28305123080b11d3c79aa569b044ba
naren <naren@Naren-PC>
2019-12-09 12:57:19 +0530

    ss

--- src/Split.java
+++ src/Split.java
@@ -1,12 +1,15 @@
 import java.util.Arrays;
 public class Split {
 	public static void main(String[] args) {
+		
 		String s="java by narendra.txt";
-		String[] s1=s.split("a");
+		String[] s1=s.split("a");	
 		System.out.println(Arrays.toString(s1));
-		String[] s2=s.split("n");
+		String[] s2=s.split("n");		
 		System.out.println(Arrays.toString(s2));
-		String[] s3=s.split("\\.");
+		String[] s3=s.split("\\.");		
 		System.out.println(Arrays.toString(s3));
+		
+		
 	}
 }
